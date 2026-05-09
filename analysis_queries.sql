c6d7a1a2ad41759ed69a5e8dde8647ddf9230db6
rehaan <md.rehaan.shaikh2@gmail.com>
2026-05-09 19:07:20 +0530

    Add SQL scripts for analysis, dashboard, validation, and table creation

---
+++ analysis_queries.sql
@@ -0,0 +1,109 @@
+--1. Sales Analysis
+--Monthly Revenue Trend
+SELECT
+    DATE_TRUNC('month', o.order_purchase_timestamp) AS month,
+    ROUND(SUM(op.payment_value), 2) AS revenue
+FROM orders o
+JOIN order_payments op
+    ON o.order_id = op.order_id
+GROUP BY month
+ORDER BY month;
+
+--Top Revenue Categories
+SELECT
+    ct.product_category_name_english AS category,
+    ROUND(SUM(oi.price), 2) AS revenue
+FROM order_items oi
+JOIN products p
+    ON oi.product_id = p.product_id
+JOIN category_translation ct
+    ON p.product_category_name = ct.product_category_name
+GROUP BY category
+ORDER BY revenue DESC
+LIMIT 10;
+
+--2. Customer Analysis
+--Top Customer States
+SELECT
+    customer_state,
+    COUNT(customer_id) AS total_customers
+FROM customers
+GROUP BY customer_state
+ORDER BY total_customers DESC;
+
+--Repeat Customers
+SELECT
+    customer_id,
+    COUNT(order_id) AS total_orders
+FROM orders
+GROUP BY customer_id
+HAVING COUNT(order_id) > 1
+ORDER BY total_orders DESC;
+
+--3. Delivery Analysis
+--Average Delivery Days
+SELECT
+    ROUND(
+        AVG(
+            DATE_PART(
+                'day',
+                order_delivered_customer_date -
+                order_purchase_timestamp
+            )
+        ),
+        2
+    ) AS avg_delivery_days
+FROM orders
+WHERE order_delivered_customer_date IS NOT NULL;
+
+--Delayed Orders Percentage
+SELECT
+    ROUND(
+        100.0 * SUM(
+            CASE
+                WHEN order_delivered_customer_date >
+                     order_estimated_delivery_date
+                THEN 1
+                ELSE 0
+            END
+        ) / COUNT(*),
+        2
+    ) AS delayed_percentage
+FROM orders
+WHERE order_delivered_customer_date IS NOT NULL;
+
+--4. Review Analysis
+--Review Score Distribution
+SELECT
+    review_score,
+    COUNT(*) AS total_reviews
+FROM order_reviews
+GROUP BY review_score
+ORDER BY review_score;
+
+--Average Review Score by Category
+SELECT
+    ct.product_category_name_english AS category,
+    ROUND(AVG(orv.review_score), 2) AS avg_review_score
+FROM order_reviews orv
+JOIN orders o
+    ON orv.order_id = o.order_id
+JOIN order_items oi
+    ON o.order_id = oi.order_id
+JOIN products p
+    ON oi.product_id = p.product_id
+JOIN category_translation ct
+    ON p.product_category_name = ct.product_category_name
+GROUP BY category
+ORDER BY avg_review_score DESC
+LIMIT 10;
+
+--5. Payment Analysis
+--Most Used Payment Method
+SELECT
+    payment_type,
+    COUNT(*) AS usage_count
+FROM order_payments
+GROUP BY payment_type
+ORDER BY usage_count DESC;
+
